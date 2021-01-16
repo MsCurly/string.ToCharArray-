ca36dbeab48c37e99858a5d9a334c7057f3088f5
MsCurly <zuleyha_98@live.com>
2021-01-16 15:07:06 +0300

    ilk gonderme

---
+++ string.ToCharArray()/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @string.ToCharArray__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String kelime = "Kodlamayı Öğreniyoruz";
+            char[] chars = kelime.ToCharArray(0, 3);
+
+            Console.Write("Girlen kelimenin ilk üç harfini ve onların indeksini verir");
+            Console.WriteLine("\nKelime: " + kelime);
+
+            for (int i = 0; i < chars.Length; i++)
+                Console.WriteLine("Indeks numarası " + (i+1) + " : " + chars[i]);
+
+            Console.ReadLine();
+        }
+    }
+}
